dce27911698a7907293502c811c75b2ec95b5d26
Jayakorn Karikan <jayakornk@gmail.com>
2021-02-13 09:29:34 +0700

    🐛 Change db env

--- services/database.ts
+++ services/database.ts
@@ -4,7 +4,7 @@ function connect(): void {
   if (mongoose.connection.readyState === 0) {
     mongoose.connect(
       `${
-        process.env.MONGO_URL || 'mongodb://localhost:27017'
+        process.env.NEXTAUTH_DATABASE_URL || 'mongodb://localhost:27017'
       }/urlshortener-next`,
       {
         useCreateIndex: true,
