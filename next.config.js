c7c9da03eb08e24f74f6c58a9644890c296bff7c
Jayakorn Karikan <jayakornk@gmail.com>
2021-02-14 17:11:38 +0700

    ⚡️ Enable next tree-shaking

--- next.config.js
+++ next.config.js
@@ -1,13 +1,23 @@
-module.exports = {
-  images: {
-    domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/r/:path*',
-        destination: '/api/r/:path*',
-      },
-    ];
-  },
-};
+/* eslint-disable @typescript-eslint/no-var-requires */
+const withTM = require('next-transpile-modules')(['@geist-ui/react']);
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALYZE === 'true',
+});
+
+module.exports = withTM(
+  withBundleAnalyzer({
+    reactStrictMode: true,
+    poweredByHeader: false,
+    images: {
+      domains: ['avatars.githubusercontent.com', 'lh3.googleusercontent.com'],
+    },
+    async rewrites() {
+      return [
+        {
+          source: '/r/:path*',
+          destination: '/api/r/:path*',
+        },
+      ];
+    },
+  })
+);
