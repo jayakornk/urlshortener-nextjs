03b69a2ec45e5d1399b6eb9b0d9484f77c592200
Jayakorn Karikan <jayakornk@gmail.com>
2021-02-14 23:23:09 +0700

    ✨ Add link to footer

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,12 @@ const Home = ({
   return (
     <div className="container mx-auto">
       <div className="px-4">
+        <h1 className="mb-2 text-2xl font-semibold tracking-wide text-gray-700 lg:mb-4 lg:text-4xl">
+          Let&apos;s shorten the link{' '}
+          <span role="img" aria-label="lightning bolt">
+            ⚡
+          </span>
+        </h1>
         <Form />
         <Table urls={urls} />
       </div>
